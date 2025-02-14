cacee5fe15671b80e6f4d0149480bc99c42b67fe
BugHunter-X <askzenai@gmail.com>
2025-02-14 11:28:45 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,23 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error in Next.js 15 if not handled correctly
+  throw new Error('Something went wrong!');
+}
+```
